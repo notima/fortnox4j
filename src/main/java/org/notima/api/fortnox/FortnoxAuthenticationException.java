42320b2c69459b6e68baab64ed893ea6c5cd95ef
Daniel Tamm <daniel@notima.se>
2022-09-22 10:52:30 +0200

    Extend FortnoxAuthenticationException

--- src/main/java/org/notima/api/fortnox/FortnoxAuthenticationException.java
+++ src/main/java/org/notima/api/fortnox/FortnoxAuthenticationException.java
@@ -1,9 +1,30 @@
 package org.notima.api.fortnox;
 
+import org.notima.api.fortnox.clients.FortnoxCredentials;
+
 public class FortnoxAuthenticationException extends Exception {
 
-    public FortnoxAuthenticationException(String format) {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = -1144464080938537096L;
+
+	private FortnoxCredentials credentials;
+	
+	public FortnoxAuthenticationException(String format) {
         super(format);
     }
-    
+
+	public boolean hasCredentials() {
+		return (credentials!=null);
+	}
+	
+	public FortnoxCredentials getCredentials() {
+		return credentials;
+	}
+
+	public void setCredentials(FortnoxCredentials credentials) {
+		this.credentials = credentials;
+	}
+	
 }
