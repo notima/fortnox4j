a22fb7a6271be69a0cd7ed92f337c0485ab0560c
Daniel Tamm <daniel@notima.se>
2023-01-18 16:30:00 +0100

    Add OrderInterface

--- src/main/java/org/notima/api/fortnox/entities3/Order.java
+++ src/main/java/org/notima/api/fortnox/entities3/Order.java
@@ -4,7 +4,7 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement(name="Order")
-public class Order {
+public class Order implements OrderInterface {
 
 	private double administrationFee;
 	private double administrationFeeVat;
